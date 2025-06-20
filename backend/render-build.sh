08fcaf213d6767b3085e8b1dab10077a7a26d076
Yitayew A <yaworkineh24@gmail.com>
2025-06-20 18:43:45 -0400

    Render sh file

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,2 +1,8 @@
+# apt-get update && apt-get install -y tesseract-ocr
+# pip install -r requirements.txt
+
+#!/usr/bin/env bash
+echo "✅ Starting render-build.sh"
 apt-get update && apt-get install -y tesseract-ocr
+which tesseract
 pip install -r requirements.txt
